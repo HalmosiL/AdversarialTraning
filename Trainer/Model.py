4c3537c4d3040b6e705e95a9b8f4e3089ece41a3
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-01 12:16:37 +0200

    Update Model.py

--- Trainer/Model.py
+++ Trainer/Model.py
@@ -1,6 +1,23 @@
 import segmentation_models_pytorch as smp
 import torch
 
+def get_DeepLabv3(device, encoder_weights=None):
+    model = smp.DeepLabV3(
+        encoder_name='resnet34',
+        encoder_depth=5,
+        encoder_weights=encoder_weights,
+        decoder_channels=256,
+        in_channels=3,
+        classes=19,
+        activation=None,
+        upsampling=8,
+        aux_params=None
+    )
+    
+    model = model.to(device)
+    
+    return model
+
 def get_model_dummy(device):
     return torch.nn.Sequential(
         torch.nn.Conv2d(3, 19, 3, stride=1, padding=1),
