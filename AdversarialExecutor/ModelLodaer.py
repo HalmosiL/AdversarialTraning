cf00c3ab2fd9e2a2763d40a8d7ed420594bf5664
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-01 12:28:47 +0200

    Update ModelLodaer.py

--- AdversarialExecutor/ModelLodaer.py
+++ AdversarialExecutor/ModelLodaer.py
@@ -5,6 +5,23 @@ def resnet_slice_model(model, level="Encoder"):
     if(level == "Encoder"):
         return torch.nn.Sequential(model.encoder)
 
+def get_DeepLabv3(device, encoder_weights=None):
+    model = smp.DeepLabV3(
+        encoder_name='resnet34',
+        encoder_depth=5,
+        encoder_weights=encoder_weights,
+        decoder_channels=256,
+        in_channels=3,
+        classes=19,
+        activation=None,
+        upsampling=8,
+        aux_params=None
+    )
+
+    model = model.to(device)
+
+    return model
+
 def get_resnet18_hourglass(device, encoder_weights=None):
     model = smp.Unet(
         encoder_name="resnet18",
