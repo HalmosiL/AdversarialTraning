6f9a570fc46d2672cedb74bb758764de3adede6d
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-02 13:20:11 +0200

    Update Dataset.py

--- AdversarialLoader/Dataset.py
+++ AdversarialLoader/Dataset.py
@@ -12,9 +12,6 @@ class DatasetAdversarial:
         self.epoch = 0
 
     def __getitem__(self, idx):
-        images = []
-        labels = []
-
         if(idx + 1 == self.len_dataset and self.plus_batch_num != None):
             concatenate_number_actual = self.plus_batch_num
         else:
@@ -23,25 +20,27 @@ class DatasetAdversarial:
         idx_ = idx * concatenate_number_actual + self.epoch * (self.len_dataset - 1)
         i = 0
         
+        images = []
+        labels = []
+        
         images_remove = []
         labels_remove = []
         
         while(i < concatenate_number_actual):
-            if(len(self.data_in_queue)):
-                image_path = self.data_queue_path + "image_" + str(idx_ + i + 1) + ".pt"
-                label_path = self.data_queue_path + "label_" + str(idx_ + i + 1) + ".pt"
+            image_path = self.data_queue_path + "image_" + str(idx_ + i + 1) + ".pt"
+            label_path = self.data_queue_path + "label_" + str(idx_ + i + 1) + ".pt"
 
-                if(
-                    os.path.exists(image_path) and
-                    os.path.exists(label_path)
-                ):
-                    images.append(torch.load(image_path).clone())
-                    labels.append(torch.load(label_path).clone())
-                    
-                    images_remove.append(image_path)
-                    labels_remove.append(label_path)
-                    
-                    i += 1
+            if(
+                os.path.exists(image_path) and
+                os.path.exists(label_path)
+            ):
+                images.append(torch.load(image_path).clone())
+                labels.append(torch.load(label_path).clone())
+
+                images_remove.append(image_path)
+                labels_remove.append(label_path)
+
+                i += 1
             else:
                 time.sleep(0.5)
                 self.data_in_queue = glob.glob(self.data_queue_path + "image_*")
