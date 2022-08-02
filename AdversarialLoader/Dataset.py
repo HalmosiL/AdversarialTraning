12a618eedf8410e211ae4a467bd294e042f069f0
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-02 23:19:16 +0200

    Update Dataset.py

--- AdversarialLoader/Dataset.py
+++ AdversarialLoader/Dataset.py
@@ -3,13 +3,16 @@ import torch
 import time
 
 class DatasetAdversarial:    
-    def __init__(self, data_queue_path, len_dataset, concatenate_number, plus_batch_num):
+    def __init__(self, data_queue_path, len_dataset, concatenate_number, plus_batch_num, type_):
         self.len_dataset = len_dataset
         self.concatenate_number = concatenate_number
         self.data_queue_path = data_queue_path
         self.plus_batch_num = plus_batch_num
-        self.epoch = 0
-        self.delete_q = []
+        
+        if(type_ == "train")
+            self.path_queue = "../AdversarialExecutor/train_queue.json"
+        else:
+            self.path_queue = "../AdversarialExecutor/val_queue.json"
 
     def __getitem__(self, idx):
         if(idx + 1 == self.len_dataset and self.plus_batch_num != None):
@@ -17,31 +20,39 @@ class DatasetAdversarial:
         else:
             concatenate_number_actual = self.concatenate_number
 
-        idx_ = idx * concatenate_number_actual + self.epoch * (self.len_dataset - 1)
         i = 0
         
         images = []
         labels = []
-        
-        images_remove = []
-        labels_remove = []
-        
+
         count_no_data = 0
         
         while(i < concatenate_number_actual):
-            image_path = self.data_queue_path + "image_" + str(idx_ + i + 1) + ".pt"
-            label_path = self.data_queue_path + "label_" + str(idx_ + i + 1) + ".pt"
+            idx_ = None
+            
+            with open(self.path_queue, 'r+') as f:
+                data = json.load(f)
+                if(len(data['IDS']) != 0):
+                    idx_ = data['IDS'][0]
+                    data['IDS'].pop(0)
+                    f.seek(0)
+                    json.dump(data, f)
+                    f.truncate()
+            
+            image_path = self.data_queue_path + "image_" + str(idx_) + ".pt"
+            label_path = self.data_queue_path + "label_" + str(idx_) + ".pt"
 
             if(
+                idy_ is not None and
                 os.path.exists(image_path) and
                 os.path.exists(label_path)
             ):
                 count_no_data = 0
                 images.append(torch.load(image_path).clone())
                 labels.append(torch.load(label_path).clone())
-
-                images_remove.append(image_path)
-                labels_remove.append(label_path)
+                
+                os.remove(image_path[-1])
+                os.remove(labels[-1])
 
                 i += 1
             else:
@@ -55,18 +66,6 @@ class DatasetAdversarial:
 
         images = torch.cat(images, dim=0)
         labels = torch.cat(labels, dim=0)
-        
-        self.delete_q.append([images_remove, labels_remove])
-        
-        if(len(self.delete_q) > 5):
-            for i in range(len(self.delete_q[0][0])):
-                os.remove(self.delete_q[0][0][i])
-                os.remove(self.delete_q[0][1][i])
-                
-            self.delete_q.pop(0)
-            
-        if(idx == self.len_dataset - 1):
-            self.epoch += 1
 
         return images, labels
 
