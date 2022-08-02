dbffe36cf69dabdd61aacf43b25464065b385917
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-02 15:01:11 +0200

    Update Dataset.py

--- AdversarialLoader/Dataset.py
+++ AdversarialLoader/Dataset.py
@@ -41,7 +41,7 @@ class DatasetAdversarial:
 
                 i += 1
             else:
-                time.sleep(1)
+                time.sleep(0.01)
 
         images = torch.cat(images, dim=0)
         labels = torch.cat(labels, dim=0)
