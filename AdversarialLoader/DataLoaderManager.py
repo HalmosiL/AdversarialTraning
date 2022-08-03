1bbe0355dd64afd1b6bb55d2a6b34932c2245da9
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-03 13:12:36 +0200

    Create DataLoaderManager.py

---
+++ AdversarialLoader/DataLoaderManager.py
@@ -0,0 +1,49 @@
+import json
+import os
+
+class SingletonClass(object):
+  def __new__(cls):
+    if not hasattr(cls, 'instance'):
+      cls.instance = super(SingletonClass, cls).__new__(cls)
+    return cls.instance
+
+class DataLoaderManager(SingletonClass):
+  ID_GETER_IS_FREE = True
+  
+  def getID(self, path_queue):
+    if(not ID_GETER_IS_FREE):
+      return []
+    
+    ID_GETER_IS_FREE = False
+    
+    with open(path_queue, 'r+') as f:
+      data = json.load(f)
+      
+      if(len(data['IDS']) != 0):
+          idx_ = data['IDS'][0]
+          image_path = self.data_queue_path + "image_" + str(idx_) + ".pt"
+          label_path = self.data_queue_path + "label_" + str(idx_) + ".pt"
+          
+          if(
+              os.path.exists(image_path) and
+              os.path.exists(label_path)
+          ):
+            print(data['IDS'])
+            data['IDS'].pop(0)
+            
+            f.seek(0)
+            json.dump(data, f)
+            f.truncate()
+            f.close()
+            
+            ID_GETER_IS_FREE = True
+            return [image_path, label_path]
+            
+        f.seek(0)
+        json.dump(data, f)
+        f.truncate()
+        f.close()
+
+        ID_GETER_IS_FREE = True
+        return []
+  
