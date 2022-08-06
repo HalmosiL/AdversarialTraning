0553966e81f96252b499e6ae170bbf3a1dc6d94f
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-06 09:59:00 +0200

    Update Gen.py

--- AdversarialExecutor/Gen.py
+++ AdversarialExecutor/Gen.py
@@ -1,7 +1,7 @@
 from Adversarial import model_immer_attack_auto_loss
 import torch
 
-def run(id_, batch, device, model, attack, number_of_steps, data_queue):
+def run(id_, batch, device, model, attack, number_of_steps, data_queue, split):
     print("Gen_", id_, " started..")
 
     image = batch[0].to(device)
@@ -15,5 +15,14 @@ def run(id_, batch, device, model, attack, number_of_steps, data_queue):
         device=device
     )
 
-    torch.save(image.cpu().detach(), data_queue + 'image_' + str(id_) + '.pt')
-    torch.save(label.cpu().detach(), data_queue + 'label_' + str(id_) + '.pt')
+    if(split == -1 or split == 1):
+        torch.save(image.cpu().detach(), data_queue + 'image_' + str(id_) + '_0_.pt')
+        torch.save(label.cpu().detach(), data_queue + 'label_' + str(id_) + '_0_.pt')
+    else:
+        image = torch.split(image.cpu().detach(), split)
+        label = torch.split(image.cpu().detach(), split)
+        
+        for i in range(split):
+            torch.save(image[i], data_queue + 'image_' + str(id_) + '_' + str(i) + '_.pt')
+            torch.save(label[i], data_queue + 'label_' + str(id_) + '_' + str(i) + '_.pt')
+
