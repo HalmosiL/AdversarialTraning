6e30317875feffdd6f8784a905572c646bd75daf
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-10 09:58:58 +0200

    Update Gen.py

--- AdversarialExecutor/Gen.py
+++ AdversarialExecutor/Gen.py
@@ -28,7 +28,7 @@ def run(id_, batch, device, model, attack, number_of_steps, data_queue, split):
         print(label[0].shape)
         print(split)
         
-        for i in range(t_split):
+        for i in range(split):
             torch.save(image[i].cpu().detach().clone(), data_queue + 'image_' + str(id_) + '_' + str(i) + '_.pt')
             torch.save(label[i].cpu().detach().clone(), data_queue + 'label_' + str(id_) + '_' + str(i) + '_.pt')
 
