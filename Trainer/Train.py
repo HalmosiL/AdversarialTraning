3c5b3aa0bb57aaf0b65b63ac5ebb336d0c675df4
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-17 11:54:03 +0200

    Update Train.py

--- Trainer/Train.py
+++ Trainer/Train.py
@@ -76,6 +76,7 @@ def train(CONFIG_PATH, CONFIG, DEVICE, train_loader_adversarial, val_loader_adve
         
         for batch_id, data in enumerate(train_loader_adversarial):
             if(len(data) == 3):
+                print("step")
                 image = data[0][0].to(DEVICE)
                 target = data[1][0].to(DEVICE)
 
