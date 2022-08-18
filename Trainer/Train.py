6b0c4fc7cce7879430a88691d340300041629901
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-08-18 10:49:44 +0200

    Update Train.py

--- Trainer/Train.py
+++ Trainer/Train.py
@@ -89,6 +89,7 @@ def train(CONFIG_PATH, CONFIG, DEVICE, train_loader_adversarial, val_loader_adve
                 
                 output, main_loss, aux_loss, _ = model(image, target)
                 loss = main_loss + CONFIG['AUX_WEIGHT'] * aux_loss
+                print("loss:", loss)
                 print(output.get_device())
                 print(output.shape)
                 
